e6545317a000586c434f2cdc4479d28546baf5fe
John Trammell <johntrammell@gmail.com>
2012-11-29 13:08:34 -0600

    added mysql pkgs

--- bin/ubuntu-deps.sh
+++ bin/ubuntu-deps.sh
@@ -1,13 +1,18 @@
+#!/bin/sh
+
 apt-get install make gcc autoconf bison re2c libxpm-dev libsasl2-dev \
     libpspell-dev libreadline5-dev libsnmp-dev
 
 apt-get install apache2 apache2-mpm-prefork apache2-prefork-dev \
     apache2-utils apache2.2-common
 
+apt-get install mysql-common mysql-server mysql-server-5.0 mysql-client \
+    mysql-client-5.0 mysql-server-core-5.0
+
 apt-get install aspell libaspell-dev libxml2 libsnmp-base libsnmp15 \
     libxml2-dev libbz2-dev libzip-dev libzip1 zlib1g zlib1g-dev \
     curl libcurl4-openssl-dev libcurl3 libcurl3-gnutls \
     libc-client-dev libfreetype6 libfreetype6-dev \
     libjpeg62 libjpeg62-dev libpng12-0 libpng12-dev \
-    libmcrypt-dev libmcrypt4 libtidy-dev libxslt1-dev \
+    libmcrypt-dev libmcrypt4 libtidy-dev libxslt1-dev
 
